8e08a69484e888639ae9658bfd50b3b65f469ed6
Jennifer <jennifer.gd42@gmail.com>
2017-01-09 23:29:01 -0800

    Added route to toggle LED. (Hopefully...)

--- server.py
+++ server.py
@@ -6,20 +6,31 @@ import time
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
+led = LED(17)
+led.on()
+
+
 @app.route('/')
 def start_here():
     """Home page."""
 
-    led = LED(17)
-    led.on()
-
     return render_template("homepage.html")
 
-@app.route("/led"):
+
+@app.route("/led")
 def change_led_status():
     """Turn the LED on or off."""
+
+    if led.is_lit:
+        led.off()
+
+    else:
+        led.on()
+
+    return "/led"
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(host='192.168.3.248', port=80, debug=True)
+    # app.run(host='192.168.3.248', port=80, debug=True)
+    app.run(debug=True)
